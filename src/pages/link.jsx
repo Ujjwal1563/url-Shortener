b4bcd9579d57dac1296e111733a9710a14141c06
Ujjwal Sharma <101029502+Ujjwal1563@users.noreply.github.com>
2024-07-04 07:58:16 +0530

    new

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -80,7 +80,7 @@ const LinkPage = () => {
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+            https://trimrrr.in/{link}
           </a>
           <a
             href={url?.original_url}
